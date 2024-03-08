76c4056357984390a2e0ea81d4b08e9d8aec5c2c
PatricioBalardini <patriciobalardini@gmail.com>
2024-03-08 01:25:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Header from "./components/Navbar";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font bebas">
+      <Header />
+      <Footer />
+    </main>
+  );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 md:stiky button-0 z-50">
+      <div className="container mx-auto floex flex-wrap p-5 flex-col md:flex-row items-center">
+        <h3>Cohort</h3>
+      </div>
+    </footer>
+  );
+}
